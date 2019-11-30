3dd506f02112843dcfb4407a1cfd72297e7f18b0
idcas <cpineda@idcas.edu.do>
2019-11-30 19:48:16 -0400

    Agregando envio de correo a traves de la notificación de correo

--- js/main.js
+++ js/main.js
@@ -56,13 +56,51 @@ jQuery(document).on('ready', function ($) {
     /*---------------------------
         MICHIMP INTEGRATION
     -----------------------------*/
-    $('#mc-form').ajaxChimp({
+    /*$('#mc-form').ajaxChimp({
         url: 'http://devitfamily.us14.list-manage.com/subscribe/post?u=a77a312486b6e42518623c58a&amp;id=8e9f692d44', //Set Your Mailchamp URL
         callback: function (resp) {
             if (resp.result === 'success') {
                 $('.subscriber-form input, .subscriber-form button').hide();
             }
         }
+    });*/
+    const notificame = document.querySelector('.plus-btn');
+    const form = document.querySelector('#mc-form');
+    const label = form.querySelector('label');
+    const input = form.querySelector('input');
+
+    const isEmailValid = function(email) {
+      let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+
+    notificame.addEventListener('click', () => {
+      label.textContent = '';
+        if(input.value === ''){
+          label.textContent = 'Debes completar el campo';
+          return;
+        } else if  (! isEmailValid(input.value)) {
+          label.textContent = 'Debes ingresar un correo electrónico válido';
+          return;
+        }
+
+        Email.send({
+            /*Host : "smtp.idcas.edu.do",
+            Username : "cpineda@idcas.edu.do",
+            Password : "C@v(uVa8",*/
+            SecureToken: '7059cc5a-eddf-4a3d-96f8-aca0bc03ab13',
+            To : "cpineda@idcas.edu.do",
+            From : "cpineda@idcas.edu.do",
+            Subject : "Listado de Emails",
+            Body : "Hola Como estás? me gustaría que me notificame cuando la página este lista <br> <strong>mi correo electrónico es</strong>: "
+                + input.value
+        }).then(
+          message => alert(message)
+        );
+        input.value = '';
+        label.textContent = 'Muchas gracias por enviarnos tu correo electrónico';
+
+
     });
 
 
@@ -74,7 +112,7 @@ jQuery(document).on('ready', function ($) {
         removalDelay: 500, //delay removal by X to allow out-animation
         callbacks: {
             beforeOpen: function () {
-                // just a hack that adds mfp-anim class to markup 
+                // just a hack that adds mfp-anim class to markup
                 this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
                 this.st.mainClass = this.st.el.attr('data-effect');
             }
